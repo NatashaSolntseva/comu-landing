c034c94489a4d849414c9755beb360b25f43e9be
NatashaSolntseva <84683371+NatashaSolntseva@users.noreply.github.com>
2023-09-23 12:58:48 +0300

    fix: header markup

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,22 @@
-window.addEventListener('DOMContentLoaded', () => {
-    console.log('start')
-})
+window.addEventListener("DOMContentLoaded", () => {
+  console.log("start");
+});
 
+function openBurgerMenu() {
+  //console.log("open");
+  document.getElementById("overlayBurgerMenu").style.top = "0px";
+  document.body.style.overflow = "hidden";
+}
+
+const overlay = document.getElementById("overlayBurgerMenu");
+
+overlay.addEventListener("click", function (evt) {
+  if (evt.target === overlay) {
+    closeBurgerMenu();
+  }
+});
+
+function closeBurgerMenu() {
+  document.getElementById("overlayBurgerMenu").style.top = "-100vh";
+  document.body.style.overflow = "auto";
+}
