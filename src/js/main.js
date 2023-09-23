172190cc059568175ccec81b7cdabffa894db32c
NatashaSolntseva <84683371+NatashaSolntseva@users.noreply.github.com>
2023-09-23 14:22:53 +0300

    fix: bugs fixed

--- src/js/main.js
+++ src/js/main.js
@@ -2,10 +2,14 @@ window.addEventListener("DOMContentLoaded", () => {
   console.log("start");
 });
 
+let scrollYPosition = 0;
+
 function openBurgerMenu() {
   //console.log("open");
+  scrollYPosition = window.scrollY;
   document.getElementById("overlayBurgerMenu").style.top = "0px";
   document.body.style.overflow = "hidden";
+  modal.style.display = "block";
 }
 
 const overlay = document.getElementById("overlayBurgerMenu");
@@ -19,4 +23,6 @@ overlay.addEventListener("click", function (evt) {
 function closeBurgerMenu() {
   document.getElementById("overlayBurgerMenu").style.top = "-100vh";
   document.body.style.overflow = "auto";
+  modal.style.display = "none";
+  window.scrollTo(0, scrollYPosition);
 }
